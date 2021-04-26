d06c8fe567bed0db9d64c28cde67fb96d56ff23c
Marco Chilese <marco.chilese@studenti.unipd.it>
2021-04-26 18:22:39 +0200

    Roll back

--- cmd/refresh/refresh.go
+++ cmd/refresh/refresh.go
@@ -13,7 +13,6 @@ import (
 	"log"
 	"net/url"
 	"os"
-	"os/exec"
 	"os/signal"
 	"path"
 	"runtime"
@@ -58,24 +57,6 @@ func main() {
 	}()
 
 	ctx, fail := ctxutils.WithFail(context.Background())
-	
-	commandArgs := []string{"13", "main"} // for creating  the cluster 
-        cmd := exec.CommandContext(ctx, "pg_createcluster", commandArgs...)
-        var cmdStderr bytes.Buffer
-        cmd.Stderr = &cmdStderr
-        log.Print(cmd.String())
-        if err = cmd.Run(); err != nil {
-                log.Print(cmdStderr.String())
-        }
-        
-        commandArgs2 := []string{"start"} // for creating  the cluster 
-        cmd2 := exec.CommandContext(ctx, "/etc/init.d/postgresql", commandArgs2...)
-        var cmdStderr2 bytes.Buffer
-        cmd2.Stderr = &cmdStderr2
-        log.Print(cmd2.String())
-        if err = cmd2.Run(); err != nil {
-                log.Print(cmdStderr2.String())
-        }
 
 	_, err := nationalization.New(lang)
 	if err != nil {
